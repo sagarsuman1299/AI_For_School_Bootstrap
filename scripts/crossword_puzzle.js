057b3ddeeff7e7022dfefa12688df81059206839
Bbiswabasu <58428413+Bbiswabasu@users.noreply.github.com>
2021-08-15 14:36:07 +0530

    basic games added

---
+++ scripts/crossword_puzzle.js
@@ -0,0 +1,104 @@
+Module.onRuntimeInitialized = _ => {
+    //declare all variables
+    var grid_size, grid;
+    var cross_gen, CSPify;
+    function addBox(type) {
+        var element = document.createElement("input");
+        element.setAttribute("type", "text");
+        element.setAttribute("value", "");
+
+        if (type === '#') {
+            element.setAttribute("disabled", "disabled");
+            element.style.cssText = `
+              background-color:black;
+               width:30px; 
+               height:30px;
+             `;
+        }
+        else {
+            element.style.cssText = `
+              background-color:white;
+               width:30px;
+               height:30px; 
+             `;
+        }
+        document.getElementById("crossword_area").append(element);
+    }
+    document.getElementById("crossword_area").innerHTML = "";
+    cross_gen = new Module.CrosswordGenerator();
+    //take input from user
+    cross_gen.grid_size =9;
+    grid_size = cross_gen.grid_size;
+    //generate grid
+    cross_gen.do_all_tasks();
+    grid = cross_gen.grid;
+    CSPify = new Module.CSPify();
+    CSPify.do_all_tasks();
+
+    for (var i = 1; i <= grid_size; i++) {
+        for (var j = 1; j <= grid_size; j++) {
+            if (grid.get(i).get(j) != 35)
+                addBox('.');
+            else
+                addBox('#');
+        }
+        document.getElementById("crossword_area").innerHTML = document.getElementById("crossword_area").innerHTML + "<br/>";
+    }
+    document.getElementById("question_area").setAttribute("style", "visiblity:visible");
+
+    document.getElementById("add_node").onclick = function add_node() {
+        var list_elem = document.createElement("li");
+        var row_text = document.createElement("input");
+        row_text.setAttribute("type", "text");
+        row_text.setAttribute("value", "");
+        row_text.setAttribute("style", "width:20px");
+        list_elem.append(row_text);
+
+        var column_text = document.createElement("input");
+        column_text.setAttribute("type", "text");
+        column_text.setAttribute("value", "");
+        column_text.setAttribute("style", "width:20px");
+        list_elem.append(column_text);
+
+        var direction_text = document.createElement("input");
+        direction_text.setAttribute("type", "text");
+        direction_text.setAttribute("value", "");
+        direction_text.setAttribute("style", "width:20px");
+        list_elem.append(direction_text);
+
+        var delete_button = document.createElement("button");
+        delete_button.textContent = "delete";
+        list_elem.append(delete_button);
+        delete_button.onclick = function deletefromlist() {
+            delete_button.parentElement.parentElement.removeChild(delete_button.parentElement);
+        }
+
+        list_elem.setAttribute("style", "padding:10px");
+        document.getElementById("nodes_list").append(list_elem);
+    }
+    document.getElementById("btn_check").onclick = function check() {
+        var findCrosswordNode = new Module.FindCrosswordNodes();
+
+        var ol = document.getElementById("nodes_list");
+        for (var i = 0; i < ol.children.length;) {
+            var row = parseInt(ol.children[i].children[0].value), col = parseInt(ol.children[i].children[1].value);
+            findCrosswordNode.add_node(row, col, (ol.children[i].children[2].value).charCodeAt(0));
+            i++;
+        }
+
+        var ans = findCrosswordNode.check();
+        var wrong_nodes = findCrosswordNode.wrong_nodes;
+        var missed_nodes = findCrosswordNode.missed_nodes;
+        var correct_nodes = findCrosswordNode.correct_nodes;
+        var result_area = document.getElementById("result_area");
+        for (var i = 0; i < correct_nodes.size(); i++) {
+            result_area.innerHTML += "You marked correctly : " + correct_nodes.get(i).first.first + "-" + correct_nodes.get(i).first.second + "-" + String.fromCharCode(correct_nodes.get(i).second) + "<br/>";
+        }
+        for (var i = 0; i < wrong_nodes.size(); i++) {
+            result_area.innerHTML += "You marked wrong : " + wrong_nodes.get(i).first.first + "-" + wrong_nodes.get(i).first.second + "-" + String.fromCharCode(wrong_nodes.get(i).second) + "<br/>";
+        }
+        for (var i = 0; i < missed_nodes.size(); i++) {
+            result_area.innerHTML += "You missed : " + missed_nodes.get(i).first.first + "-" + missed_nodes.get(i).first.second + "-" + String.fromCharCode(missed_nodes.get(i).second) + "<br/>";
+        }
+    }
+};
---
+++ scripts/expression_evaluation.js
@@ -0,0 +1,142 @@
+Module.onRuntimeInitialized = _ => {
+    dag_gen = new Module.DAGGenerator();
+    exp_eval = new Module.ExpressionEvaluation();
+    match_exp = new Module.MatchExpression();
+
+    //take input from user
+    num_vars = 2;
+    num_nodes = 5;
+
+    //generate the dag and fetch their values
+    dag_gen.num_nodes = num_nodes;
+    dag_gen.num_vars = num_vars;
+    dag_gen.do_all_tasks();
+    var content = dag_gen.content;
+    var j = 0;
+    while (content.get(j) != null) {
+        if (content.get(j) === "=>")
+            content.set(j, "⇒");
+        else if (content.get(j) === "<=>")
+            content.set(j, "⇔");
+        j++;
+    }
+    num_nodes = dag_gen.num_nodes;
+
+    values = dag_gen.values;
+    adj = dag_gen.adj;
+    depth = dag_gen.depth;
+
+    //compute the expression
+    match_exp.init();
+    match_exp.compute_expressions(0);
+    expressions = dag_gen.expressions;
+
+    //display question
+    var question = "Evaluate the expression<br/>" + expressions.get(0) + "<br/>when ";
+    for (var i = 0; i < dag_gen.num_nodes; i++) {
+        if (content.get(i).charCodeAt(0) >= 97 && content.get(i).charCodeAt(0) <= 122)
+            question = question + content.get(i) + " = " + values.get(i) + " , ";
+    }
+    document.getElementById("question_area").innerHTML = question;
+
+    //compute values
+    exp_eval.init();
+    answer = exp_eval.evaluate(0);
+    values = dag_gen.values;
+    order_of_evaluation = exp_eval.order_of_evaluation;
+    //graph building starts
+    build_graph(num_vars, num_nodes, content, adj); 
+
+    document.getElementById("btn_check").onclick = function check_ans() {
+        var student_answer = parseInt(document.getElementById("student_answer").value);
+        if (student_answer !== 0 && student_answer !== 1)
+           document.getElementById("result_area").innerHTML = "This is not boolean value";
+        else {
+           if (answer == student_answer)
+              document.getElementById("result_area").innerHTML = "CORRECT";
+           else
+              document.getElementById("result_area").innerHTML = "WRONG";
+        }
+     }
+     var ptr = 0;
+     document.getElementById("btn_visualize").onclick = function visualize() {
+        var node = order_of_evaluation.get(ptr);
+        cy.getElementById(node).data("name", values.get(node));
+        ptr++;
+     }
+     function build_graph(num_vars, num_nodes, content, adj) {
+
+        cy = cytoscape({
+
+           container: document.getElementById('cy'), // container to render in
+           style: [ // the stylesheet for the graph
+              {
+                 selector: 'node',
+                 style: {
+                    'background-color': '#666',
+                    'color': 'white',
+                    'label': 'data(name)',
+                    'width': '40px',
+                    'height': '40px',
+                    'text-valign': 'center',
+                    'text-halign': 'center'
+                 }
+              },
+
+              {
+                 selector: 'edge',
+                 style: {
+                    'width': 2,
+                    'line-color': 'blue',
+                    'target-arrow-color': 'blue',
+                    'target-arrow-shape': 'triangle',
+                    'curve-style': 'unbundled-bezier',
+                    'control-point-weight': '0.5',
+                    'control-point-distance': '0',
+
+                 }
+              }
+           ],
+
+        });
+
+        var arr = new Array(num_nodes).fill(0);
+        x_coor = dag_gen.x_coor;
+        y_coor = dag_gen.y_coor;
+        edge_carvature = dag_gen.edge_carvature;
+
+        //add nodes
+        for (var i = 0; i < num_nodes; i++) {
+           cy.add([
+              { group: 'nodes', data: { id: i, name: content.get(i) }, position: { x: 100 * (x_coor.get(i) + 0.2), y: 100 * (y_coor.get(i) + 1) } }
+           ]);
+        }
+        arr = null;
+
+        //add edges
+        for (var i = 0; i < num_nodes; i++) {
+           var j = 0;
+           while (adj.get(i).get(j) != null) {
+              var tar = adj.get(i).get(j);
+              cy.add([
+                 { group: 'edges', data: { id: i + 't' + tar, source: i, target: tar } }
+              ]);
+              if (content.get(i) == "~") {
+                 cy.getElementById(i + 't' + tar).style("line-color", "#000");
+                 cy.getElementById(i + 't' + tar).style("target-arrow-color", "#000");
+              }
+              else if (j == 0) {
+                 cy.getElementById(i + 't' + tar).style("line-color", "red");
+                 cy.getElementById(i + 't' + tar).style("target-arrow-color", "red");
+              }
+
+              //bend edges if they pass through nodes
+              cy.getElementById(i + 't' + tar).style("control-point-weight", "0.5");
+              cy.getElementById(i + 't' + tar).style("control-point-distance", -20 * edge_carvature.get(i).get(j));
+
+              j++;
+           }
+        }
+     }
+
+};
